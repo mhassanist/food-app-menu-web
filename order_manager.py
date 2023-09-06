4d4b8907efced02148a47fbe3f22b43d92c03581
Mohammed S. Hassan <m.hassan@ogr.iu.edu.tr>
2023-09-06 17:51:59 +0300

    Create order_manager.py

---
+++ order_manager.py
@@ -0,0 +1,52 @@
+import requests
+
+menu = [{
+  "name": "Burger",
+  "price": 5.0,
+  "available": True
+}, {
+  "name": "Pizza",
+  "price": 8.0,
+  "available": True
+}, {
+  "name": "Pasta",
+  "price": 6.0,
+  "available": True
+}, {
+  "name": "Salad",
+  "price": 4.0,
+  "available": True
+}]
+
+order = []
+
+
+def get_menu_items():
+
+  url = 'https://gist.githubusercontent.com/mhassanist/ebea3d06c5dc942e190a69a8330d4906/raw/923a8cbedd175652abcdc53a0f805d7082cdc3fc/menu.json'
+
+  resp = requests.get(url)
+  data = resp.json()  # Check the JSON Response Content documentation below
+  menu = data['menu']
+
+  return menu
+
+
+def get_order_total():
+  total = 0
+  for item in order:
+    total += item['price'] * item['qty']
+
+  print("total: ", total)
+  return total
+
+
+def add_order_item(item, qty):
+  new_order_item = {"name": item['name'], "price": item['price'], "qty": qty}
+
+  #new_order_item_tuple = (item['name'], item['name'], qty)
+  #new_order_item_list = [item['name'], item['name'], qty]
+
+  order.append(new_order_item)
+
+  print("order", order)
